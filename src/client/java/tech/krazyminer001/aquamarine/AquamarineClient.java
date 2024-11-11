8ba3bdc7780f1e87c77a7a096fb759d1cbaf6fd8
KrazyMiner001 <krazyminer001@outlook.com>
2024-11-11 10:21:09 +0200

    feat: Add template

---
+++ src/client/java/tech/krazyminer001/aquamarine/AquamarineClient.java
@@ -0,0 +1,10 @@
+package tech.krazyminer001.aquamarine;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AquamarineClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/tech/krazyminer001/aquamarine/AquamarineDataGenerator.java
@@ -0,0 +1,11 @@
+package tech.krazyminer001.aquamarine;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AquamarineDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
