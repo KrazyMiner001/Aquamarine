af0f0613d0581b2e4fd4f36fb7176941c0ce5789
KrazyMiner001 <krazyminer001@outlook.com>
2024-11-14 16:53:13 +0200

    refactor: Improved block registration code

--- src/main/java/tech/krazyminer001/aquamarine/example/ExampleBlocks.java
+++ src/main/java/tech/krazyminer001/aquamarine/example/ExampleBlocks.java
@@ -1,16 +1,13 @@
 package tech.krazyminer001.aquamarine.example;
 
-import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityType;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.loot.LootTables;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -18,11 +15,11 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import tech.krazyminer001.aquamarine.Aquamarine;
 
-import java.util.Optional;
+import java.util.function.Function;
 
 public class ExampleBlocks {
     public static final Block EXAMPLE_MULTIBLOCK = register("example_multiblock",
-            new ExampleMultiblock(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(Aquamarine.MOD_ID, "example_multiblock")))), true);
+            ExampleMultiblock::new, AbstractBlock.Settings.copy(Blocks.IRON_BLOCK), true);
 
     public static final BlockEntityType<ExampleMultiblockBlockEntity> EXAMPLE_MULTIBLOCK_BLOCK_ENTITY_BLOCK_ENTITY_TYPE = register("example_block_entity",
             FabricBlockEntityTypeBuilder.create(ExampleMultiblockBlockEntity::new, EXAMPLE_MULTIBLOCK).build()
@@ -32,11 +29,15 @@ public class ExampleBlocks {
         return Registry.register(Registries.BLOCK_ENTITY_TYPE, Identifier.of(Aquamarine.MOD_ID, id), type);
     }
 
-    private static <T extends Block> T register(String name, T block, boolean registerItem) {
+    private static <T extends Block> T register(String name, Function<AbstractBlock.Settings, T> blockFunction, AbstractBlock.Settings blockSettings, boolean registerItem) {
+        Identifier identifier = Identifier.of(Aquamarine.MOD_ID, name);
+        RegistryKey<Block> blockRegistryKey = RegistryKey.of(RegistryKeys.BLOCK, identifier);
+        T block = Registry.register(Registries.BLOCK, identifier, blockFunction.apply(blockSettings.registryKey(blockRegistryKey)));
         if (registerItem) {
-            Registry.register(Registries.ITEM, Identifier.of(Aquamarine.MOD_ID, name), new BlockItem(block, new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Aquamarine.MOD_ID, "example_multiblock")))));
+            RegistryKey<Item> itemRegistryKey = RegistryKey.of(RegistryKeys.ITEM, identifier);
+            Registry.register(Registries.ITEM, identifier, new BlockItem(block, new Item.Settings().useBlockPrefixedTranslationKey().registryKey(itemRegistryKey)));
         }
-        return Registry.register(Registries.BLOCK, Identifier.of(Aquamarine.MOD_ID, name), block);
+        return block;
     }
 
     public static void init() {}
