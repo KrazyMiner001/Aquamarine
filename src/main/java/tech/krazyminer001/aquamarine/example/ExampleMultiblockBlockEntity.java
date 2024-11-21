dff254a4d2ae8341761d9a3c732ee06cf7c63a43
KrazyMiner001 <krazyminer001@outlook.com>
2024-11-21 16:49:36 +0200

    feat: Moved more methods to MultiblockBlockEntity class

--- src/main/java/tech/krazyminer001/aquamarine/example/ExampleMultiblockBlockEntity.java
+++ src/main/java/tech/krazyminer001/aquamarine/example/ExampleMultiblockBlockEntity.java
@@ -3,28 +3,23 @@ package tech.krazyminer001.aquamarine.example;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.NotNull;
 import tech.krazyminer001.aquamarine.Aquamarine;
-import tech.krazyminer001.aquamarine.blocks.FastBlockEntity;
 import tech.krazyminer001.aquamarine.multiblocks.*;
 import tech.krazyminer001.aquamarine.multiblocks.inventory.AquaInventory;
 import tech.krazyminer001.aquamarine.multiblocks.inventory.ConfigurableFluidStack;
 import tech.krazyminer001.aquamarine.multiblocks.inventory.ConfigurableItemStack;
 import tech.krazyminer001.aquamarine.multiblocks.inventory.MultiblockInventory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ExampleMultiblockBlockEntity extends MultiblockBlockEntity {
-    private final List<HatchBlockEntity> hatches = new ArrayList<>();
     private final MultiblockInventory multiblockInventory = new MultiblockInventory();
 
     public ExampleMultiblockBlockEntity(BlockPos pos, BlockState state) {
         super(ExampleBlocks.EXAMPLE_MULTIBLOCK_BLOCK_ENTITY_BLOCK_ENTITY_TYPE, pos, state);
     }
 
-    private AquaInventory inventory = new AquaInventory(
+    private final AquaInventory inventory = new AquaInventory(
             List.of(new ConfigurableItemStack()),
             List.of(new ConfigurableFluidStack(1000))
     );
@@ -33,7 +28,10 @@ public class ExampleMultiblockBlockEntity extends MultiblockBlockEntity {
             .add(0, 1,  0, SimpleMember.ofBlock(() -> Blocks.GLASS), new HatchFlags.Builder().with(HatchType.FLUID_INPUT).build())
             .build();
 
-    private ShapeMatcher matcher = null;
+    @Override
+    public MultiblockInventory getMultiblockInventory() {
+        return multiblockInventory;
+    }
 
     @Override
     public ShapeTemplate getShapeTemplate() {
@@ -41,41 +39,13 @@ public class ExampleMultiblockBlockEntity extends MultiblockBlockEntity {
     }
 
     @Override
-    public MultiblockInventory getMultiblockInventory() {
-        return multiblockInventory;
-    }
-
-    @Override
     protected void tick() {
-        assert world != null;
-        if (world.isClient()) return;
-        link();
-
-        if (hatches != matcher.getMatchedHatches()) {
-            hatches.clear();
-            hatches.addAll(matcher.getMatchedHatches());
-        }
+        super.tick();
 
         //Aquamarine.LOGGER.info("Multiblock does{} match", matcher.isMatchSuccessful() ? "" : "n't");
 
         //Aquamarine.LOGGER.info("Multiblock has {} hatches", hatches.size());
         if (!multiblockInventory.getFluidInputs().isEmpty())
             Aquamarine.LOGGER.info("Multiblock has {} {}s", multiblockInventory.getFluidInputs().getFirst().getAmount(), multiblockInventory.getFluidInputs().getFirst().getResource().getFluid());
-    }
-
-    public final void link() {
-        assert world != null;
-        if (matcher == null) {
-            matcher = new ShapeMatcher(world, pos, world.getBlockState(pos).get(ExampleMultiblock.FACING), template);
-            matcher.registerListeners(world);
-        }
-
-        if (matcher.needsRematch()) {
-            matcher.rematch(world);
-        }
-
-        if (matcher.isMatchSuccessful()) {
-            multiblockInventory.recollectIO(matcher);
-        }
     }
 }
--- src/main/java/tech/krazyminer001/aquamarine/multiblocks/MultiblockBlockEntity.java
+++ src/main/java/tech/krazyminer001/aquamarine/multiblocks/MultiblockBlockEntity.java
@@ -2,14 +2,19 @@ package tech.krazyminer001.aquamarine.multiblocks;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import tech.krazyminer001.aquamarine.blocks.FastBlockEntity;
 import tech.krazyminer001.aquamarine.multiblocks.inventory.MultiblockInventory;
 
-public abstract class MultiblockBlockEntity extends FastBlockEntity {
+import java.util.ArrayList;
+import java.util.List;
 
+public abstract class MultiblockBlockEntity extends FastBlockEntity {
+    private ShapeMatcher matcher = null;
+    private final List<HatchBlockEntity> hatches = new ArrayList<>();
 
     public MultiblockBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
@@ -19,10 +24,41 @@ public abstract class MultiblockBlockEntity extends FastBlockEntity {
 
     public abstract MultiblockInventory getMultiblockInventory();
 
+    protected ShapeMatcher getShapeMatcher() {return matcher;}
+
+    protected void setShapeMatcher(ShapeMatcher matcher) {
+        this.matcher = matcher;
+    }
+
     public static void tick(@NotNull World world, BlockPos pos, BlockState state, MultiblockBlockEntity entity) {
         if (world.isClient()) return;
         entity.tick();
     }
 
-    protected void tick() {}
+    public final void link() {
+        assert world != null;
+        if (matcher == null) {
+            matcher = new ShapeMatcher(world, pos, world.getBlockState(pos).get(Properties.FACING), getShapeTemplate());
+            matcher.registerListeners(world);
+        }
+
+        if (matcher.needsRematch()) {
+            matcher.rematch(world);
+        }
+
+        if (matcher.isMatchSuccessful()) {
+            getMultiblockInventory().recollectIO(matcher);
+        }
+    }
+
+    protected void tick() {
+        assert world != null;
+        if (world.isClient()) return;
+        link();
+
+        if (hatches != matcher.getMatchedHatches()) {
+            hatches.clear();
+            hatches.addAll(matcher.getMatchedHatches());
+        }
+    }
 }
