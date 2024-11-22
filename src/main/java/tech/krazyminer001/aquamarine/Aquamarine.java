ec829eca290e5237c9f501131ff42219b63d84a7
KrazyMiner001 <krazyminer001@outlook.com>
2024-11-22 15:27:40 +0200

    feat: Only register example blocks if in development environment

--- src/main/java/tech/krazyminer001/aquamarine/Aquamarine.java
+++ src/main/java/tech/krazyminer001/aquamarine/Aquamarine.java
@@ -1,33 +1,18 @@
 package tech.krazyminer001.aquamarine;
 
-import com.mojang.brigadier.arguments.ArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.minecraft.block.Blocks;
-import net.minecraft.command.argument.ArgumentTypes;
-import net.minecraft.command.argument.BlockPosArgumentType;
-import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.entity.player.PlayerEntity;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.krazyminer001.aquamarine.example.ExampleBlocks;
-import tech.krazyminer001.aquamarine.multiblocks.ShapeTemplate;
-import tech.krazyminer001.aquamarine.multiblocks.SimpleMember;
 import tech.krazyminer001.aquamarine.multiblocks.world.ChunkEventListeners;
 import tech.krazyminer001.aquamarine.networking.AquamarineData;
-import tech.krazyminer001.aquamarine.networking.AquamarineS2CPacketSender;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import static net.minecraft.server.command.CommandManager.argument;
-import static net.minecraft.server.command.CommandManager.literal;
 
 public class Aquamarine implements ModInitializer {
 	public static final String MOD_ID = "aquamarine";
@@ -55,8 +40,10 @@ public class Aquamarine implements ModInitializer {
 
 		ChunkEventListeners.init();
 
-		ExampleBlocks.init();
-	}
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            ExampleBlocks.init();
+        }
+    }
 
 	static {
 		messages = new ArrayList<>();
