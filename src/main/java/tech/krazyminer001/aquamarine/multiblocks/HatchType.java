90f4e9d3becf574a7bcba8811b4f4eb8a2820f28
KrazyMiner001 <krazyminer001@outlook.com>
2024-11-23 15:47:26 +0200

    feat: Made hatch types use identifiers instead of strings and changed ENERGY hatches to TR_ENERGY

--- src/main/java/tech/krazyminer001/aquamarine/multiblocks/HatchType.java
+++ src/main/java/tech/krazyminer001/aquamarine/multiblocks/HatchType.java
@@ -2,7 +2,9 @@ package tech.krazyminer001.aquamarine.multiblocks;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.util.Identifier;
+
+import static tech.krazyminer001.aquamarine.util.Utilities.of;
 
 /**
  * Interface that deals with hatch types. To add extra hatch types create an enum that extends this class.
@@ -11,31 +13,35 @@ public class HatchType {
       public static final PacketCodec<ByteBuf, HatchType> PACKET_CODEC = new PacketCodec<>() {
         @Override
         public void encode(ByteBuf buf, HatchType value) {
-            PacketCodecs.STRING.encode(buf, value.toString());
+            Identifier.PACKET_CODEC.encode(buf, value.getId());
         }
 
         @Override
         public HatchType decode(ByteBuf buf) {
-            return new HatchType(PacketCodecs.STRING.decode(buf));
+            return new HatchType(Identifier.PACKET_CODEC.decode(buf));
         }
     };
 
-    private final String name;
+    private final Identifier id;
 
-    protected HatchType(String name) {
-        this.name = name;
+    protected HatchType(Identifier id) {
+        this.id = id;
+    }
+
+    public Identifier getId() {
+        return id;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof HatchType)) {return false;}
-        return name.equals(((HatchType) obj).name);
+        return id.equals(((HatchType) obj).id);
     }
 
-    public static final HatchType ITEM_INPUT = new HatchType("ITEM_INPUT");
-    public static final HatchType ITEM_OUTPUT = new HatchType("ITEM_OUTPUT");
-    public static final HatchType FLUID_INPUT = new HatchType("FLUID_INPUT");
-    public static final HatchType FLUID_OUTPUT = new HatchType("FLUID_OUTPUT");
-    public static final HatchType ENERGY_INPUT = new HatchType("ENERGY_INPUT");
-    public static final HatchType ENERGY_OUTPUT = new HatchType("ENERGY_OUTPUT");
+    public static final HatchType ITEM_INPUT = new HatchType(of("ITEM_INPUT"));
+    public static final HatchType ITEM_OUTPUT = new HatchType(of("ITEM_OUTPUT"));
+    public static final HatchType FLUID_INPUT = new HatchType(of("FLUID_INPUT"));
+    public static final HatchType FLUID_OUTPUT = new HatchType(of("FLUID_OUTPUT"));
+    public static final HatchType TR_ENERGY_INPUT = new HatchType(of("TR_ENERGY_INPUT"));
+    public static final HatchType TR_ENERGY_OUTPUT = new HatchType(of("TR_ENERGY_OUTPUT"));
 }
